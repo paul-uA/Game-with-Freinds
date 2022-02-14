4f7dcb133151b97d3eac1f0800e4a7e959a3068d
paulua <paulavila54mac@gmail.com>
2022-02-13 22:53:21 -0600

    working onnsearch page  and buttons for the search function

--- src/components/gameSearch.jsx
+++ src/components/gameSearch.jsx
@@ -1,16 +1,49 @@
 
+import './gameSearch.css'
+
 function GameSearch (props) {
 
+const tagList = [ 'mmorpg', 'shooter', 'strategy', 'moba', 
+'racing', 'sports', 'social', 'sandbox', 'open-world', 'survival',
+ 'pvp', 'pve', 'pixel', 'voxel', 'zombie', 'turn-based', 'first-person', 
+ 'third-Person', 'top-down', 'tank', 'space', 'sailing', 'side-scroller', 
+ 'superhero', 'permadeath', 'card', 'battle-royale', 'mmo', 'mmofps',
+  'mmotps', '3d', '2d', 'anime', 'fantasy', 'sci-fi', 'fighting', 
+  'action-rpg', 'action', 'military', 'martial-arts', 'flight', 'low-spec', 
+  'tower-defense', 'horror', 'mmorts']
 
 
+const tagButtons = tagList.map((ele,idx)=>{
 
+})
+
+//     return(
+//         <>
+//         <h1>Search Page</h1>
+
+// <div class="chip">
+//         {/* <!-- Content --> */}
+//             <div class="chip__content">
+//            button
+//             </div>
+//          <button class="button">
+//             <div>
+//                 <ul class="dots">
+//                     <li class="dots__item"></li>
+//                     {/* <!-- Repeat other dots --> */}
+//                  </ul>
+//             </div>
+//         </button>
+// </div>
+//         </>
+//     )
 
 
     return(
-        <>
-        <h1>Search Page</h1>
-        </>
+        <></>
     )
+
+
 }
 
 export default GameSearch
