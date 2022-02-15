49e85d5bdfc5634c551eda8f8a82020acf5771b5
paulua <paulavila54mac@gmail.com>
2022-02-14 19:00:12 -0600

    placing box over components for later styling with css

--- src/components/gameSearch.jsx
+++ src/components/gameSearch.jsx
@@ -1,80 +1,97 @@
-import {useState} from 'react'
+import { useState, useEffect } from 'react'
 import './gameSearch.css'
 
-function GameSearch (props) {
+function GameSearch(props) {
 
-const tagList = [ 'mmorpg', 'shooter', 'strategy', 'moba', 
-'racing', 'sports', 'social', 'sandbox', 'open-world', 'survival',
- 'pvp', 'pve', 'pixel', 'voxel', 'zombie', 'turn-based', 'first-person', 
- 'third-Person', 'top-down', 'tank', 'space', 'sailing', 'side-scroller', 
- 'superhero', 'permadeath', 'card', 'battle-royale', 'mmo', 'mmofps',
-  'mmotps', '3d', '2d', 'anime', 'fantasy', 'sci-fi', 'fighting', 
-  'action-rpg', 'action', 'military', 'martial-arts', 'flight', 'low-spec', 
-  'tower-defense', 'horror', 'mmorts']
+    const tagList = ['mmorpg', 'shooter', 'strategy', 'moba',
+        'racing', 'sports', 'social', 'sandbox', 'open-world', 'survival',
+        'pvp', 'pve', 'pixel', 'voxel', 'zombie', 'turn-based', 'first-person',
+        'third-Person', 'top-down', 'tank', 'space', 'sailing', 'side-scroller',
+        'superhero', 'permadeath', 'card', 'battle-royale', 'mmo', 'mmofps',
+        'mmotps', '3d', '2d', 'anime', 'fantasy', 'sci-fi', 'fighting',
+        'action-rpg', 'action', 'military', 'martial-arts', 'flight', 'low-spec',
+        'tower-defense', 'horror', 'mmorts']
 
-  const [urltags, setURLTags] = useState(false)
-
-
-  const toggleTag = ()=> {
-    setURLTags(!urltags);
-
-  }
+    const [urltags, setURLTags] = useState([])
+    // const [searchTag, setSearchTag] = useState([])
 
 
-const tagButtons = tagList.map((ele,idx)=>{
-    
-    return(
+    const toggleTag = (e) => {
+        const activeArray = [...urltags]
+       let tagName = e.target.textContent
+
+        if (activeArray.includes(tagName)) {
+            activeArray.splice(activeArray.indexOf(tagName), 1)
+        }
+        else {
+            activeArray.push(tagName)
+        }
+        setURLTags(activeArray)
+
+    }
+
+    const handleSubmit = (e) =>{
+        let checkbox1 = (e.target.form[45].value === 'on' )
+        let checkbox2 = e.target.form[46].value
+
+
+
+    }
+
+
+    const tagButtons = tagList.map((ele, idx) => {
+
+        return (
+            <>
+                <button name="tag" className={`chip ${urltags.includes(ele) ? "active" : ''}`} onClick={toggleTag} key={idx}>
+                    {/* <!-- Content --> */}
+                    <div className="chip__content">
+                        {ele}
+                    </div>
+                </button>
+            </>
+        )
+    })
+
+    const SubmitTags = (e) => {
+        e.preventDefault()
+
+    }
+
+    useEffect(() => {
+
+    })
+
+
+
+
+
+    return (
         <>
-        <button className="chip" on>
-         {/* <!-- Content --> */}
-             <div className="chip__content">
-            {ele}
-             </div>
-        </button>
-        </>
-    )
-})
 
+                <div className='serach-options'>
+                    <form action="GET" onSubmit={SubmitTags}>
+                        <div className='Tags-options'>
+                            {tagButtons}
+                        </div>
+                        <div className='platform-choice'>
+                        Platform
+                        <label className="label">
+                           <input type="checkbox" name='Platform' />
+                            <p className='Platform'>PC</p>
+                        </label>
+                        <label className="label">
+                            <input type="checkbox" name='Platform'  />
+                            <p className='Platform'>Browser</p>
+                        </label>
+                        <button type='submit' onClick={handleSubmit}>Search</button>
+                        </div>
+                    </form>
+                </div>
 
+            <section className='searched-games'>
 
-    return(
-        <>
-        <div className='serach-options'>
-            <form action="">
-                <div>
-            {tagButtons}
-            </div>
-            <label class="label">
-    {/* <!-- The real radio --> */}
-    <input type="radio" class="label__input" />
-
-    {/* <!-- The fake circle --> */}
-    <div class="label__circle">
-        {/* <!-- The inner circle --> */}
-        <div class="label__radio label__radio--selected"></div>
-    </div>
-
-    {/* <!-- The text --> */}
-    <p className='Platform'>PC</p>
-</label>
-<label class="label">
-    {/* <!-- The real radio --> */}
-    <input type="radio" class="label__input" />
-
-    {/* <!-- The fake circle --> */}
-    <div class="label__circle">
-        {/* <!-- The inner circle --> */}
-        <div class="label__radio label__radio--selected"></div>
-    </div>
-
-    {/* <!-- The text --> */}
-    <p className='Platform'>Browser</p>
-</label>
-
-            
-
-            </form>
-        </div>
+            </section>
         </>
     )
 
