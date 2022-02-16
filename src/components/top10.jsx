82dd3a035b1c1ea3792cab06fbf6ac2a531b9a8b
paulua <paulavila54mac@gmail.com>
2022-02-16 09:16:35 -0600

    adding card features to site

--- src/components/top10.jsx
+++ src/components/top10.jsx
@@ -1,65 +1,64 @@
 import { useEffect, useState } from "react";
-import {Route,Routes,useNavigate, Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
+import { Card, Button } from "react-bootstrap";
 
 import './topTen.css'
 
 
-function Top10Box (props) {
+function Top10Box(props) {
 
 
 
-        
+
     const [gameData, setGameData] = useState()
 
 
-    let  params = '?platform=pc&category=shooter&sort-by=popularity'
-    const URL= "https://free-to-play-games-database.p.rapidapi.com/api/games"
+    let params = '?platform=pc&category=shooter&sort-by=popularity'
+    const URL = "https://free-to-play-games-database.p.rapidapi.com/api/games"
     const options = {
         method: 'GET',
         headers: {
             'x-rapidapi-host': 'free-to-play-games-database.p.rapidapi.com',
             'x-rapidapi-key': process.env.REACT_APP_API_KEY
-            }
+        }
     };
 
-    useEffect(()=>{
-        fetch(URL+params,options)
-        .then((res)=>res.json())
-        .then((json)=>{
-            //console.log(json)
-            setGameData(json)
-            //console.log(gameData)
-        })
+    useEffect(() => {
+        fetch(URL + params, options)
+            .then((res) => res.json())
+            .then((json) => {
+                //console.log(json)
+                setGameData(json)
+                //console.log(gameData)
+            })
 
+
+    }, [])
+
+    const TenInfo = !gameData ? <p>Loading Games.....</p> : gameData.slice(0, 10).map((ele, idx) => {
+        return (
         
-    },[])
-            
-    const TenInfo = !gameData ? <p>Loading Games.....</p> : gameData.slice(0,10).map((ele,idx)=>{
-        return(
-            <div key={idx}>
-            <Link to={`/details/${ele.id}`} key={idx}>
-            <div className="card" key={idx} >
-                {/* <!-- Cover --> */}
-                <div className="card__cover">
-                    <img className='thumbNail' src={ele.thumbnail} />
-                </div>
-                {/* <!-- Content --> */}
-                <div className="card__content">
-                    <p>{ele.title}</p>
-                </div>
-                    <p className="genre-tag">{ele.genre}</p>
-            </div>
-            </Link>
-            </div>
-        )
-    }) 
+            <Card key={idx} style={{ width: '18rem' }}>
+  <Card.Img variant="top" src={ele.thumbnail} />
+  <Card.Body>
+    <Card.Title>{ele.title}</Card.Title>
+    <Card.Text>
+        {ele.short_description}
+    </Card.Text>
+    <Link to={`/details/${ele.id}`} key={idx}>
+    <Button variant="dark">Details</Button></Link>
+  </Card.Body>
+</Card>
 
-    return(
+        )
+    })
+
+    return (
         <>
-        {TenInfo}
+            {TenInfo}
         </>
     )
-        
+
 }
 
 export default Top10Box
